b40f8a599dcb3d92b51a53b1b7298e749c17efca
James Cook <jcook@lgsinnovations.com>
2017-09-01 15:26:27 -0700

    expanding to include basic coverage of complete dbus api

--- esm/index.js
+++ esm/index.js
@@ -4,6 +4,7 @@
 // See https://developer.gnome.org/NetworkManager/stable/spec.html
 
 const DBus = require('dbus-as-promised');
+const PLazy = require('p-lazy');
 
 const SERVICE_NAME = 'org.freedesktop.NetworkManager';
 
@@ -35,33 +36,23 @@ class NetworkManager extends DBus.InterfaceWrapper {
   }
 }
 
-class Device extends DBus.InterfaceWrapper {
-  static async connect(bus, objPath) {
-    const RPC_INTERFACE = 'org.freedesktop.NetworkManager.Device';
-
-    return new Device(await
-      await bus.getInterface(SERVICE_NAME, objPath, RPC_INTERFACE));
-  }
-
-  get Statistics() {
-    return (async () => {
-      if (!this._statistics) {
-        this._statistics = await DeviceStatistics.connect(
-          this._iface.bus, 
-          this._iface.objectPath);
-      }
-
-      return this._statistics;
-    })()
+class AgentManager extends DBus.InterfaceWrapper {
+  static async connect(bus) {
+    const OBJECT_PATH = '/org/freedesktop/NetworkManager/AgentManager';
+    const RPC_INTERFACE = 'org.freedesktop.NetworkManager.AgentManager';
+    
+    return new AgentManager(
+      await bus.getInterface(SERVICE_NAME, OBJECT_PATH, RPC_INTERFACE));
   }
 }
 
-class DeviceStatistics extends DBus.InterfaceWrapper {
-  static async connect(bus, objPath) {
-    const RPC_INTERFACE = 'org.freedesktop.NetworkManager.Device.Statistics';
-
-    return new DeviceStatistics(
-      await bus.getInterface(SERVICE_NAME, objPath, RPC_INTERFACE));
+class DnsManager extends DBus.InterfaceWrapper {
+  static async connect(bus) {
+    const OBJECT_PATH = '/org/freedesktop/NetworkManager/DnsManager';
+    const RPC_INTERFACE = 'org.freedesktop.NetworkManager.DnsManager';
+    
+    return new DnsManager(
+      await bus.getInterface(SERVICE_NAME, OBJECT_PATH, RPC_INTERFACE));
   }
 }
 
@@ -84,7 +75,165 @@ class Connection extends DBus.InterfaceWrapper {
   }
 }
 
+class Device extends DBus.InterfaceWrapper {
+  constructor(iface) {
+    super(iface);
+    
+    Device.subInterfaces.forEach(name => {
+      this[name] = PLazy.from(this._getSubInterface.bind(this, name));
+    });
+  }
+  
+  static async connect(bus, objPath) {
+    const RPC_INTERFACE = 'org.freedesktop.NetworkManager.Device';
+
+    return new Device(await
+      await bus.getInterface(SERVICE_NAME, objPath, RPC_INTERFACE));
+  }
+
+  async _getSubInterface(name) {
+    return Device[name].connect(this._iface.bus, this._iface.objectPath);
+  }
+}
+
+// TODO: dynamically discover which of these apply to each particular device object,
+// using bus.introspect.
+Device.subInterfaces = [
+  'Statistics',
+  'Adsl',
+  'Bond',
+  'Bridge',
+  'Bluetooth',
+  'Dummy',
+  'Wired',
+  'Generic',
+  'Infiniband',
+  'IPTunnel',
+  'Macsec',
+  'Macvlan',
+  'Modem',
+  'OlpcMesh',
+  'Team',
+  'Tun',
+  'Veth',
+  'Vlan',
+  'Vxlan',
+  'Wireless',
+  'WiMax',
+];
+Device.subInterfaces.forEach(name => {
+  Device[name] = class extends DBus.InterfaceWrapper {
+    static async connect(bus, objPath) {
+      const RPC_INTERFACE = 'org.freedesktop.NetworkManager.Device.' + name;
+      
+      return new Device[name](
+        await bus.getInterface(SERVICE_NAME, objPath, RPC_INTERFACE));
+    }
+  }
+});
+
+Device.subInterfaces.push('PPP');
+Device.PPP = class extends DBus.InterfaceWrapper {
+  static async connect(bus, objPath) {
+    const RPC_INTERFACE = 'org.freedesktop.NetworkManager.PPP';
+    
+    return new Device.PPP (
+        await bus.getInterface(SERVICE_NAME, objPath, RPC_INTERFACE));
+  }
+}
+
+
+class ActiveConnection extends DBus.InterfaceWrapper {
+  constructor(iface, vpnIface) {
+    super(iface);
+    
+    this.VPN = PLazy.from(this._getVpnInterace.bind(this));
+    this.Connection = PLazy.from(this._getConnection.bind(this));
+  }
+  
+  static async connect(bus, objPath) {
+    const RPC_INTERFACE = 'org.freedesktop.NetworkManager.Connection.Active';
+    const VPN_INTERFACE = 'org.freedesktop.NetworkManager.VPN.Connection';
+    
+    return new ActiveConnection(
+      await bus.getInterface(SERVICE_NAME, objPath, RPC_INTERFACE));
+  }
+  
+  async _getVpnInterace() {
+    return ActiveConnectionVPN.connect(this._iface.bus, this._iface.objectPath);
+  }
+  
+  async _getConnection() {
+    let objPath = await this.getProperty('Connection');
+    
+    return Connection.connect(this._iface.bus, objPath);
+  }
+}
+
+class ActiveConnectionVPN extends DBus.InterfaceWrapper {
+  static async connect(bus, objPath) {
+    const RPC_INTERFACE = 'org.freedesktop.NetworkManager.VPN.Connection';
+    
+    return new ActiveConnectionVPN(
+      await bus.getInterface(SERVICE_NAME, objPath, RPC_INTERFACE));
+  }
+}
+
+class IP4Config extends DBus.InterfaceWrapper {
+  static async connect(bus, objPath) {
+    const RPC_INTERFACE = 'org.freedesktop.NetworkManager.IP4Config';
+    
+    return new IP4Config(
+      await bus.getInterface(SERVICE_NAME, objPath, RPC_INTERFACE));
+  }
+}
+
+class IP6Config extends DBus.InterfaceWrapper {
+  static async connect(bus, objPath) {
+    const RPC_INTERFACE = 'org.freedesktop.NetworkManager.IP6Config';
+    
+    return new IP6Config(
+      await bus.getInterface(SERVICE_NAME, objPath, RPC_INTERFACE));
+  }
+}
+
+class DHCP4Config extends DBus.InterfaceWrapper {
+  static async connect(bus, objPath) {
+    const RPC_INTERFACE = 'org.freedesktop.NetworkManager.DHCP4Config';
+    
+    return new DHCP4Config(
+      await bus.getInterface(SERVICE_NAME, objPath, RPC_INTERFACE));
+  }
+}
+
+class DHCP6Config extends DBus.InterfaceWrapper {
+  static async connect(bus, objPath) {
+    const RPC_INTERFACE = 'org.freedesktop.NetworkManager.DHCP6Config';
+    
+    return new DHCP6Config(
+      await bus.getInterface(SERVICE_NAME, objPath, RPC_INTERFACE));
+  }
+}
+
+class AccessPoint extends DBus.InterfaceWrapper {
+  static async connect(bus, objPath) {
+    const RPC_INTERFACE = 'org.freedesktop.NetworkManager.AccessPoint';
+    
+    return new DHCP6Config(
+      await bus.getInterface(SERVICE_NAME, objPath, RPC_INTERFACE));
+  }
+}
+
+exports.SERVICE_NAME = SERVICE_NAME;
+
 exports.NetworkManager = NetworkManager;
-exports.Device = Device;
+exports.AgentManager = AgentManager;
+exports.DnsManager = DnsManager;
 exports.Settings = Settings;
 exports.Connection = Connection;
+exports.Device = Device;
+exports.IP4Config = IP4Config;
+exports.IP6Config = IP6Config;
+exports.DHCP4Config = DHCP4Config;
+exports.DHCP6Config = DHCP6Config;
+exports.AccessPoint = AccessPoint;
